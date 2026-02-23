333d51acf743408c26b679a9c216daae4b135d60
Gorinmag <gorobets49in@gmail.com>
2026-02-23 21:05:40 +0300

    Site for education version 2 podact

--- geology_education/settings.py
+++ geology_education/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-default-key-for-dev')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['geo-education.ru', 'www.geo-education.ru', '127.0.0.1', 'localhost', '45.139.78.178']
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
